39ceb8470b1d90d3a70fbaa34df786aa79e7f7dc
Bianca9977 <bianca99776@gmail.com>
2022-12-11 18:47:51 +0900

    Temperature Scene Interaction

---
+++ MID-Project/Assets/Scripts/TemperatureSliderControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TemperatureSliderControl : MonoBehaviour
+{
+    public GameObject overlay;
+    public Slider overlaySlider;
+    private float initialScale;
+    private Vector3 objectScale;
+    public Text PlantStatus;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialScale = transform.localScale.y;
+
+        objectScale = transform.localScale;    
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeOverlay()
+    {
+        OscMessage message = new OscMessage();
+
+        message.address = "/Direction";
+
+        var sliderValue = overlaySlider.value;
+
+        transform.localScale = new Vector3(objectScale.x, (initialScale - sliderValue), objectScale.z);
+
+        if (sliderValue > 2.5)
+        {
+            PlantStatus.text = "Plant dead";
+        }
+        else if ((sliderValue <= 2) && (sliderValue > 1.5))
+        {
+            PlantStatus.text = "Plant grow";
+        }
+    }
+
+
+}
---
+++ MID-Project/Assets/objectCheck.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class objectCheck : MonoBehaviour
+{ 
+
+    public OSC osc;
+    public GameObject canvasObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        osc.SetAddressHandler("/Object", OnReceiveObject);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnReceiveObject(OscMessage message)
+    {
+        float x = message.GetFloat(0);
+
+        Debug.Log("message " + x);
+
+       // canvasObj.SetActive(false);
+    }
+}
